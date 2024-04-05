e08bc1e989b1045bd46b231e65b681466b549111
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2024-04-05 13:25:54 +0200

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "math/2Dshapes.cpp"
+
+using namespace std;
+
+
+/* ---- NOTE ----
+ please note that this file is
+ only for debugging, always
+ leave the entire main function
+ in comments when including this
+ file in your project
+*/
+
+/*
+int main() 
+//{
+
+ 2D shapes
+
+	//cout << square(null, 5);
+	//cout << rectangle(null, 36, 2);
+	//cout << parallelogram(null, 2, 3);
+	//cout << trapezoid(30, null, 8, 5);
+	//cout << triangle(79.825,10.3,null);
+	//cout << circle(null, 4);
+	//cout << ellipse(null, 15, 8);
+	
+	return 0;		
+}
+*/
